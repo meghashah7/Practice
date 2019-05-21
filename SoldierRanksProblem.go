abd8490a9511e9d19412664a098930cfa9f62226
meghashah7 <43990397+meghashah7@users.noreply.github.com>
2019-05-21 06:13:53 +0530

    Add files via upload

---
+++ SoldierRanksProblem.go
@@ -0,0 +1,41 @@
+package main
+
+// you can also use imports, for example:
+import (
+	"fmt"
+	"sort"
+)
+
+// import "os"
+
+// you can write to stdout for debugging purposes, e.g.
+// fmt.Println("this is a debug message")
+
+func main() {
+	ranks := []int{1, 0}
+	fmt.Println(Solution(ranks))
+}
+
+func Solution(ranks []int) int {
+	// write your code in Go 1.4
+	sort.Ints(ranks)
+	fmt.Println(ranks)
+	var totalreportees, rankcount int
+
+	for i := range ranks {
+		if i == 0 {
+			rankcount = 1
+			continue
+		} else if ranks[i] == ranks[i-1] {
+			rankcount++
+		} else if ranks[i] == ranks[i-1]+1 {
+			totalreportees = totalreportees + rankcount
+			rankcount = 1
+		} else {
+			rankcount = 1
+			continue
+		}
+	}
+	return totalreportees
+
+}
