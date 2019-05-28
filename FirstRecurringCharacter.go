1aceacfca736bc5501bde59493abfd0067e9a9b0
meghashah7 <43990397+meghashah7@users.noreply.github.com>
2019-05-28 05:57:24 +0530

    First recurring character practice question

---
+++ FirstRecurringCharacter.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//givenArray := []int{2, 5, 1, 2, 3, 5, 1, 2, 4}
+	//givenArray := []int{2, 1, 1, 2, 3, 5, 1, 2, 4}
+	givenArray := []int{1, 2, 3, 4}
+	fmt.Println(givenArray)
+
+	fmt.Println(Solution(givenArray))
+
+}
+
+func Solution(givenArray []int) int {
+
+	m := make(map[int]int)
+	for _, key := range givenArray {
+		m[key]++
+		if m[key] == 2 {
+			return key
+		}
+	}
+	return 0
+}
