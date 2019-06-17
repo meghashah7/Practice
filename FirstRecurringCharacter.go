ea8942aa24044cb3d33b8810a1a5c1609449fcf5
meghashah7 <43990397+meghashah7@users.noreply.github.com>
2019-06-17 18:03:24 +0530

    Update FirstRecurringCharacter.go

--- FirstRecurringCharacter.go
+++ FirstRecurringCharacter.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-
+//Below array is for testing purpose
 	//givenArray := []int{2, 5, 1, 2, 3, 5, 1, 2, 4}
 	//givenArray := []int{2, 1, 1, 2, 3, 5, 1, 2, 4}
 	givenArray := []int{1, 2, 3, 4}
