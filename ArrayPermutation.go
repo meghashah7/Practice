d20336765b2f896f9da7a04afd31ba06fef6793b
meghashah7 <43990397+meghashah7@users.noreply.github.com>
2019-06-20 12:05:37 +0530

    Update ArrayPermutation.go
    Check if the array is permutation or not.
    Codility training task

--- ArrayPermutation.go
+++ ArrayPermutation.go
@@ -1,1 +1,38 @@
+package main
 
+// you can also use imports, for example:
+//import "fmt"
+// import "os"
+import "sort"
+
+
+func main() {
+  
+  fmt.Println([]int{2,3,4,1,5,6})
+  
+}
+
+// you can write to stdout for debugging purposes, e.g.
+// fmt.Println("this is a debug message")
+
+func Solution(A []int) int {
+    // write your code in Go 1.4
+    
+    sort.Ints(A)
+  //  fmt.Println(A)
+    
+    for i:=1; i<=len(A);i++{
+        if A[i-1]==i {
+         continue   
+        } else {
+            return 0
+        }
+        
+        
+    }
+    return 1
+    
+    
+    
+
+}
